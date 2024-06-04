3f1a8af0ab7b8319f3dc8b8acd6bb9f4bd542773
조현준 <45157196+koboolean@users.noreply.github.com>
2024-06-04 15:12:42 +0900

    컴포넌트 내 훅 중 useState을 이용하여 playerName 부분 변경하기

--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -1,13 +1,28 @@
 import {Component} from "react";
+import {useState} from "react";
 
 export default function Player({player, symbol}) {
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick() {
+        console.log(isEditing);
+        setIsEditing(!isEditing);
+    }
+
+    let playerName = <span className='player-name'>{player}</span>;
+
+    if(isEditing){
+        playerName = <input type="text" required/>;
+    }
+
     return (
         <li>
               <span className='player'>
-                <span className='player-name'>{player}</span>
+                {playerName}
                 <span className='player-symbol'>{symbol}</span>
               </span>
-            <button>Edit</button>
+            {/*<button onClick={() => handleEditClick()}>Edit</button>*/}
+            <button onClick={handleEditClick}>Edit</button>
         </li>
     )
 }
