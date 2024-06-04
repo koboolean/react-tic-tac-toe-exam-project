53ba781b9f7eb0619895cf78ec3675c400b378de
조현준 <45157196+koboolean@users.noreply.github.com>
2024-06-04 14:58:58 +0900

    중복되는 부분을 컴포넌트로 분리하여 Player 나누기

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Player from "./Components/Player.jsx";
 
 function App() {
   
@@ -6,20 +7,8 @@ function App() {
       <main>
         <div id="game-container">
           <ol id="players">
-            <li>
-              <span className='player'>
-                <span className='player-name'>Player 1</span>
-                <span className='player-symbol'>X</span>
-              </span>
-              <button>Edit</button>
-            </li>
-            <li>
-              <span className='player'>
-                <span className='player-name'>Player 2</span>
-                <span className='player-symbol'>O</span>
-              </span>
-              <button>Edit</button>
-            </li>
+            <Player player="Player 1" symbol="X"/>
+            <Player player="Player 2" symbol="O"/>
           </ol>
 
           GAME BOARD
---
+++ src/Components/Player.jsx
@@ -0,0 +1,13 @@
+import {Component} from "react";
+
+export default function Player({player, symbol}) {
+    return (
+        <li>
+              <span className='player'>
+                <span className='player-name'>{player}</span>
+                <span className='player-symbol'>{symbol}</span>
+              </span>
+            <button>Edit</button>
+        </li>
+    )
+}
