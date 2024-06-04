f619183093c1e98ea82fe28680923bf374f26a7a
조현준 <45157196+koboolean@users.noreply.github.com>
2024-06-04 17:02:57 +0900

    useState 훅을 최대한 사용하지 않도록 하기 - o, x값을 조정하는 activePlayer를 제거하고 gameTurns로 작업을 할 때 헬퍼 함수를 만들어 추가해보기

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,44 @@ import GameBoard from "./Components/GameBoard.jsx";
 import {useState} from "react";
 import Log from "./Components/Log.jsx";
 
+function deriveActivePlayer(gameTurns){
+    let currentPlayer = 'X';
+
+    if(gameTurns.length > 0 && gameTurns[0].player === 'X'){
+        currentPlayer = 'O';
+    }
+
+    return currentPlayer;
+}
+
 function App() {
-    const [activePlayer, setActivePlayer] = useState('X');
     const [gameTurns, setGameTurns] = useState([]);
+
+    // 중복되는 기능의 useState를 제거하기 위해 아래의 값을 제거를 할 때 다음과 같이 추가가 될 수 있다.
+    //const [activePlayer, setActivePlayer] = useState('X');
+
+    /*let currentPlayer = 'X';
+
+    if(gameTurns.length > 0 && gameTurns[0].player === 'X'){
+        currentPlayer = 'O';
+    }
+    // 하지만, 아래의 코드하고 중복되는 코드이므로 이를 대치하기 위해 헬퍼함수를 사용하여 중복을 최소화한다.
+    */
+
+    const activePlayer = deriveActivePlayer(gameTurns);
+
     function handleSelectSquare(rowIndex, colIndex){
-        setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+        //setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
         setGameTurns(prevTurns => {
+            /*
             let currentPlayer = 'X';
 
             if(prevTurns.length > 0 && prevTurns[0].player === 'X'){
                 currentPlayer = 'O';
             }
+            */
+            let currentPlayer = deriveActivePlayer(prevTurns);
+
             return [
                 {square: {row: rowIndex, col: colIndex}, player: currentPlayer},
                 ...prevTurns
