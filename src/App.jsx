5077e9c9e1c38441e9aa3c08ef3ce7a7a5f20782
조현준 <45157196+koboolean@users.noreply.github.com>
2024-06-04 14:51:28 +0900

    컴포넌트 분리 전 중복되는 player 코드

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,28 @@ function App() {
   
 
   return (
-  <p>Comming Soon..</p>
+      <main>
+        <div id="game-container">
+          <ol id="players">
+            <li>
+              <span className='player'>
+                <span className='player-name'>Player 1</span>
+                <span className='player-symbol'>X</span>
+              </span>
+              <button>Edit</button>
+            </li>
+            <li>
+              <span className='player'>
+                <span className='player-name'>Player 2</span>
+                <span className='player-symbol'>O</span>
+              </span>
+              <button>Edit</button>
+            </li>
+          </ol>
+
+          GAME BOARD
+        </div>
+      </main>
   )
 }
 
