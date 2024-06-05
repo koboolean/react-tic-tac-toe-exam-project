d07689663ccf2c68a01d7484a7c025b66993ba7d
조현준 <45157196+koboolean@users.noreply.github.com>
2024-06-05 23:58:42 +0900

    마지막으로 App.jsx 다듬기

--- src/App.jsx
+++ src/App.jsx
@@ -15,19 +15,52 @@ function deriveActivePlayer(gameTurns){
     return currentPlayer;
 }
 
-const initialGameBoard = [
+function driveWinner(gameBoard, players){
+    let winner;
+
+    for(const combination of WINNING_COMBINATIONS){
+        const firstSymbol = gameBoard[combination[0].row][combination[0].column];
+        const secondSymbol = gameBoard[combination[1].row][combination[1].column];
+        const thirdSymbol = gameBoard[combination[2].row][combination[2].column];
+
+        if(firstSymbol && firstSymbol === secondSymbol && firstSymbol === thirdSymbol){
+            winner = players[firstSymbol];
+        }
+    }
+
+    return winner;
+}
+
+const PLAYERS = {
+    X: 'Player 1',
+    O: 'Player 2'
+}
+
+const INITIAL_GAME_BOARD = [
     [null, null, null],
     [null, null, null],
     [null, null, null]
 ];
 
+function driveGameBoard(gameTurns){
+    let gameBoard = [...INITIAL_GAME_BOARD.map(a => [...a])];
+
+    for(const t of gameTurns) {
+        const {square, player} = t;
+        const {row, col} = square;
+
+        gameBoard[row][col] = player;
+    }
+
+    return gameBoard;
+}
+
+
+
 function App() {
     const [gameTurns, setGameTurns] = useState([]);
 
-    const [players, setPayers] = useState({
-        X: 'Player 1',
-        O: 'Player 2'
-    });
+    const [players, setPayers] = useState(PLAYERS);
 
     // 중복되는 기능의 useState를 제거하기 위해 아래의 값을 제거를 할 때 다음과 같이 추가가 될 수 있다.
     //const [activePlayer, setActivePlayer] = useState('X');
@@ -41,27 +74,9 @@ function App() {
     */
 
     const activePlayer = deriveActivePlayer(gameTurns);
+    const gameBoard = driveGameBoard(gameTurns);
 
-    let gameBoard = [...initialGameBoard.map(a => [...a])];
-
-    for(const t of gameTurns){
-        const {square, player} = t;
-        const {row, col} = square;
-
-        gameBoard[row][col] = player;
-    }
-
-    let winner;
-
-    for(const combination of WINNING_COMBINATIONS){
-        const firstSymbol = gameBoard[combination[0].row][combination[0].column];
-        const secondSymbol = gameBoard[combination[1].row][combination[1].column];
-        const thirdSymbol = gameBoard[combination[2].row][combination[2].column];
-
-        if(firstSymbol && firstSymbol === secondSymbol && firstSymbol === thirdSymbol){
-            winner = players[firstSymbol];
-        }
-    }
+    const winner = driveWinner(gameBoard, players);
 
     const hasDraw = gameTurns.length === 9 && !winner;
 
@@ -101,8 +116,8 @@ function App() {
       <main>
         <div id="game-container">
           <ol id="players" className="highlight-player">
-            <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"} onChangeName={handlePalyerNameChange}/>
-            <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"} onChangeName={handlePalyerNameChange}/>
+            <Player initialName={PLAYERS.X} symbol="X" isActive={activePlayer === "X"} onChangeName={handlePalyerNameChange}/>
+            <Player initialName={PLAYERS.O} symbol="O" isActive={activePlayer === "O"} onChangeName={handlePalyerNameChange}/>
           </ol>
           {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRematch}/>}
           <GameBoard onSelectSquare={handleSelectSquare} gameBoard={gameBoard}/>
